a9041eb06d4cecf540ade2a03de61d2d3d59c60f
edream <>
2003-05-12 19:14:21 +0000

    Removing most files from leo files. (Preparing to move them to src, config, doc, plugins & scripts directories)

--- leo/__init__.py
+++ leo/__init__.py
@@ -1,9 +1,9 @@
 #@+leo
-#@+node:0::@file __init__.py
+#@+node:0::@file ../__init__.py
 #@+body
 #@@language python
 
 # Dummy file to make this a potential package.
 #@-body
-#@-node:0::@file __init__.py
+#@-node:0::@file ../__init__.py
 #@-leo
--- leo/sdist.bat
+++
@@ -1,4 +0,0 @@
-rem creates source distribution using setup.py
-rem cd c:\prog\LeoPy
-rem --formats=gztar,zip
-c:\python22\python setup.py sdist --force-manifest
