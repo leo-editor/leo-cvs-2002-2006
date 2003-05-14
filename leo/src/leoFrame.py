acc9c06a783194a6e872d2e741b2b8b4c3564254
edream <>
2003-05-14 12:05:14 +0000

    Update version number to 4.0a2.

--- leo/src/leoFrame.py
+++ leo/src/leoFrame.py
@@ -123,7 +123,7 @@ class LeoFrame:
 		# Sign on.
 		color = app().config.getWindowPref("log_error_color")
 		es("Leo Log Window...",color=color)
-		es("Leo 4.0 alpha 1, ",newline=0)
+		es("Leo 4.0 alpha 2, ",newline=0)
 		n1,n2,n3,junk,junk=sys.version_info
 		ver1 = "Python %d.%d.%d" % (n1,n2,n3)
 		ver2 = ", Tk " + self.top.getvar("tk_patchLevel")
@@ -4253,7 +4253,7 @@ class LeoFrame:
 		# Doing so would add unwanted leading tabs.
 		ver = "$Revision$" # CVS will update this.
 		build = ver[10:-1] # Strip off "$Reversion" and "$"
-		version = "leo.py 4.0 alpha 1, Build " + build + ", May 12, 2003\n\n"
+		version = "leo.py 4.0 alpha 2, Build " + build + ", May 12, 2003\n\n"
 		copyright = (
 			"Copyright 1999-2003 by Edward K. Ream\n" +
 			"All Rights Reserved\n" +
