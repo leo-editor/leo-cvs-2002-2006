4283b2a23021191341d8f5321d99f5a5f90b5ef3
edream <>
2003-10-16 13:51:05 +0000

    Synch all files. Leo 4.0 final rc2

--- leo/src/leoFrame.py
+++ leo/src/leoFrame.py
@@ -83,7 +83,7 @@ class baseLeoFrame:
 		return self.ver[10:-1] # Strip off "$Reversion" and the trailing "$"
 	
 	def getSignOnLine (self):
-		return "Leo 4.0 beta 4 build %s, October 9, 2003" % self.getBuildNumber()
+		return "Leo 4.0, build %s, October 17, 2003" % self.getBuildNumber()
 		
 	def initVersion (self):
 		self.ver = "$Revision$" # CVS will update this.
