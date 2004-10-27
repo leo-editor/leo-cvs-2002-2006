43782e3de9240c6fef2b38134bf14bdb3b341451
edream <>
2004-10-27 14:03:16 +0000

    - Added underlinedTkButton class. - Use this class to define hot keys for all items. - Tab now switches focus: use Control-tab to enter a tab.

--- leo/src/leoTkinterFind.py
+++ leo/src/leoTkinterFind.py
@@ -6,6 +6,72 @@ import leoFind
 import leoTkinterDialog
 import Tkinter as Tk
 
+#@+others
+#@+node:ekr.20041025152343:class underlinedTkButton
+class underlinedTkButton(Tk.Checkbutton):
+    
+    #@    @+others
+    #@+node:ekr.20041025152712:__init__
+    def __init__(self,buttonType,parent_widget,**keywords):
+    
+        self.buttonType = buttonType
+        self.parent_widget = parent_widget
+        self.hotKey = None
+        text = keywords['text']
+    
+        #@	<< set self.hotKey if '&' is in the string >>
+        #@+node:ekr.20041025152712.2:<< set self.hotKey if '&' is in the string >>
+        index = text.find('&')
+        
+        if index > -1:
+        
+            if index == len(text)-1:
+                # The word ends in an ampersand.  Ignore it; there is no hot key.
+                text = text[:-1]
+            else:
+                self.hotKey = text [index + 1]
+                text = text[:index] + text[index+1:]
+        #@nonl
+        #@-node:ekr.20041025152712.2:<< set self.hotKey if '&' is in the string >>
+        #@nl
+        
+        # Create the button...
+    	if self.hotKey:
+            keywords['text'] = text
+            keywords['underline'] = index
+    
+        if buttonType.lower() == "button":
+            self.button = Tk.Button(parent_widget,keywords)
+        elif buttonType.lower() == "check":
+            self.button = Tk.Checkbutton(parent_widget,keywords)
+        elif buttonType.lower() == "radio":
+            self.button = Tk.Radiobutton(parent_widget,keywords)
+        else:
+            g.trace("bad buttonType")
+        
+        self.text = text # for traces
+    #@nonl
+    #@-node:ekr.20041025152712:__init__
+    #@+node:ekr.20041026080125:bindHotKey
+    def bindHotKey (self,widget):
+        
+        if self.hotKey:
+            for key in (self.hotKey.lower(),self.hotKey.upper()):
+                widget.bind("<Alt-%s>" % key,self.callback)
+    #@-node:ekr.20041026080125:bindHotKey
+    #@+node:ekr.20041025152717:callback
+    # The hot key has been hit.  Call the button's command.
+    
+    def callback (self, event):
+        
+        g.trace(self.text)
+    
+    	self.button.invoke ()
+    #@-node:ekr.20041025152717:callback
+    #@-others
+#@nonl
+#@-node:ekr.20041025152343:class underlinedTkButton
+#@+node:ekr.20041025152343.1:class leoTkinterFind
 class leoTkinterFind (leoFind.leoFind,leoTkinterDialog.leoTkinterDialog):
 
     """A class that implements Leo's tkinter find dialog."""
@@ -71,6 +137,33 @@ class leoTkinterFind (leoFind.leoFind,leoTkinterDialog.leoTkinterDialog):
         # Use bigger boxes for scripts.
         self.find_text   = ftxt = Tk.Text(fpane,bd=1,relief="groove",height=4,width=20)
         self.change_text = ctxt = Tk.Text(cpane,bd=1,relief="groove",height=4,width=20)
+        #@<< Bind Tab and control-tab >>
+        #@+node:ekr.20041026092141:<< Bind Tab and control-tab >>
+        def setFocus(w):
+            w.focus_set()
+            g.app.gui.setSelectionRange(w,"1.0","1.0")
+            return "break"
+            
+        def toFind(event,w=ftxt): return setFocus(w)
+        def toChange(event,w=ctxt): return setFocus(w)
+            
+        def insertTab(w):
+            data = g.app.gui.getSelectionRange(w)
+            if data: start,end = data
+            else: start = end = g.app.gui.getInsertPoint(w)
+            g.app.gui.replaceSelectionRangeWithText(w,start,end,"\t")
+            return "break"
+        
+        def insertFindTab(event,w=ftxt): return insertTab(w)
+        def insertChangeTab(event,w=ctxt): return insertTab(w)
+        
+        ftxt.bind("<Tab>",toChange)
+        ctxt.bind("<Tab>",toFind)
+        ftxt.bind("<Control-Tab>",insertFindTab)
+        ctxt.bind("<Control-Tab>",insertChangeTab)
+        #@nonl
+        #@-node:ekr.20041026092141:<< Bind Tab and control-tab >>
+        #@nl
         
         fBar = Tk.Scrollbar(fpane,name='findBar')
         cBar = Tk.Scrollbar(cpane,name='changeBar')
@@ -102,42 +195,48 @@ class leoTkinterFind (leoFind.leoFind,leoTkinterDialog.leoTkinterDialog):
         
         for i in xrange(numberOfColumns):
             columns[i].pack(side="left",padx="1p") # fill="y" Aligns to top. padx expands columns.
+            
+        # HotKeys used for check/radio buttons:  a,b,c,e,h,i,l,m,n,o,p,r,s,t,w
         
         radioLists[0] = [
-            (self.dict["radio-find-type"],"Plain Search","plain-search"),  
-            (self.dict["radio-find-type"],"Pattern Match Search","pattern-search"),
-            (self.dict["radio-find-type"],"Script Search","script-search")]
+            (self.dict["radio-find-type"],"P&Lain Search","plain-search"),  
+            (self.dict["radio-find-type"],"&Pattern Match Search","pattern-search"),
+            (self.dict["radio-find-type"],"&Script Search","script-search")]
         checkLists[0] = [
-            ("Script Change",self.dict["script_change"])]
+            ("Scrip&t Change",self.dict["script_change"])]
         checkLists[1] = [
-            ("Whole Word",  self.dict["whole_word"]),
-            ("Ignore Case", self.dict["ignore_case"]),
-            ("Wrap Around", self.dict["wrap"]),
-            ("Reverse",     self.dict["reverse"])]
+            ("&Whole Word",  self.dict["whole_word"]),
+            ("&Ignore Case", self.dict["ignore_case"]),
+            ("Wrap &Around", self.dict["wrap"]),
+            ("&Reverse",     self.dict["reverse"])]
         radioLists[2] = [
-            (self.dict["radio-search-scope"],"Entire Outline","entire-outine"),
-            (self.dict["radio-search-scope"],"Suboutline Only","suboutline-only"),  
-            (self.dict["radio-search-scope"],"Node Only","node-only"),
+            (self.dict["radio-search-scope"],"&Entire Outline","entire-outine"),
+            (self.dict["radio-search-scope"],"Suboutline &Only","suboutline-only"),  
+            (self.dict["radio-search-scope"],"&Node Only","node-only"),
             # I don't know what selection-only is supposed to do.
             (self.dict["radio-search-scope"],"Selection Only",None)] #,"selection-only")]
         checkLists[2] = []
         checkLists[3] = [
-            ("Search Headline Text", self.dict["search_headline"]),
-            ("Search Body Text",     self.dict["search_body"]),
-            ("Mark Finds",           self.dict["mark_finds"]),
-            ("Mark Changes",         self.dict["mark_changes"])]
+            ("Search &Headline Text", self.dict["search_headline"]),
+            ("Search &Body Text",     self.dict["search_body"]),
+            ("&Mark Finds",           self.dict["mark_finds"]),
+            ("Mark &Changes",         self.dict["mark_changes"])]
         
         for i in xrange(numberOfColumns):
             for var,name,val in radioLists[i]:
-                box = Tk.Radiobutton(columns[i],anchor="w",text=name,variable=var,value=val)
-                box.pack(fill="x")
-                box.bind("<1>", self.resetWrap)
-                if val == None: box.configure(state="disabled")
-            for name, var in checkLists[i]:
-                box = Tk.Checkbutton(columns[i],anchor="w",text=name,variable=var)
-                box.pack(fill="x")
-                box.bind("<1>", self.resetWrap)
-                if var is None: box.configure(state="disabled")
+                box = underlinedTkButton("radio",columns[i],anchor="w",text=name,variable=var,value=val)
+                box.button.pack(fill="x")
+                box.button.bind("<1>", self.resetWrap)
+                if val == None: box.button.configure(state="disabled")
+                box.bindHotKey(ftxt)
+                box.bindHotKey(ctxt)
+            for name,var in checkLists[i]:
+                box = underlinedTkButton("check",columns[i],anchor="w",text=name,variable=var)
+                box.button.pack(fill="x")
+                box.button.bind("<1>", self.resetWrap)
+                box.bindHotKey(ftxt)
+                box.bindHotKey(ctxt)
+                if var is None: box.button.configure(state="disabled")
         #@nonl
         #@-node:ekr.20031218072017.3903:<< Create four columns of radio and checkboxes >>
         #@nl
@@ -149,23 +248,44 @@ class leoTkinterFind (leoFind.leoFind,leoTkinterDialog.leoTkinterDialog):
         buttons.pack (anchor="n",expand=1,fill="x")
         buttons2.pack(anchor="n",expand=1,fill="x")
         
-        # Create the first row of buttons
-        findButton=Tk.Button(buttons,width=8,text="Find",bd=4,command=self.findButton) # The default.
-        contextBox=Tk.Checkbutton(buttons,anchor="w",text="Show Context",variable=self.dict["batch"])
-        findAllButton=Tk.Button(buttons,width=8,text="Find All",command=self.findAllButton)
+        # HotKeys used for check/radio buttons:  a,b,c,e,h,i,l,m,n,o,p,r,s,t,w
+        # HotKeys used for plain buttons (enter),d,g,t
         
-        findButton.pack   (pady="1p",padx="25p",side="left")
-        contextBox.pack   (pady="1p",           side="left",expand=1)
-        findAllButton.pack(pady="1p",padx="25p",side="right",fill="x",)
+        # Create the first row of buttons
+        findButton=Tk.Button(buttons,
+            width=9,text="Find",bd=4,command=self.findButton) # The default.
+        findButton.pack(pady="1p",padx="25p",side="left")
+        
+        contextBox = underlinedTkButton("check",buttons,
+            anchor="w",text="Show Conte&xt",variable=self.dict["batch"])
+        contextBox.button.pack(pady="1p",side="left",expand=1)
+        contextBox.bindHotKey(ftxt)
+        contextBox.bindHotKey(ctxt)
+        
+        findAllButton = underlinedTkButton("button",buttons,
+            width=9,text="Fin&d All",command=self.findAllButton)
+        findAllButton.button.pack(pady="1p",padx="25p",side="right",fill="x")
+        findAllButton.bindHotKey(ftxt)
+        findAllButton.bindHotKey(ctxt)
         
         # Create the second row of buttons
-        changeButton    =Tk.Button(buttons2,width=8,text="Change",command=self.changeButton)
-        changeFindButton=Tk.Button(buttons2,        text="Change, Then Find",command=self.changeThenFindButton)
-        changeAllButton =Tk.Button(buttons2,width=8,text="Change All",command=self.changeAllButton)
+        changeButton = underlinedTkButton("button",buttons2,
+            width=10,text="Chan&Ge",command=self.changeButton)
+        changeButton.button.pack(pady="1p",padx="25p",side="left")
+        changeButton.bindHotKey(ftxt)
+        changeButton.bindHotKey(ctxt)
         
-        changeButton.pack    (pady="1p",padx="25p",side="left")
-        changeFindButton.pack(pady="1p",           side="left",expand=1)
-        changeAllButton.pack (pady="1p",padx="25p",side="right")
+        changeFindButton = underlinedTkButton("button",buttons2,
+            text="Change, &Then Find",command=self.changeThenFindButton)
+        changeFindButton.button.pack(pady="1p",side="left",expand=1)
+        changeFindButton.bindHotKey(ftxt)
+        changeFindButton.bindHotKey(ctxt)
+            
+        changeAllButton = underlinedTkButton("button",buttons2,
+            width=10,text="Change All",command=self.changeAllButton)
+        changeAllButton.button.pack(pady="1p",padx="25p",side="right")
+        changeAllButton.bindHotKey(ftxt)
+        changeAllButton.bindHotKey(ctxt)
         #@nonl
         #@-node:ekr.20031218072017.3905:<< Create two rows of buttons >>
         #@nl
@@ -305,5 +425,8 @@ class leoTkinterFind (leoFind.leoFind,leoTkinterDialog.leoTkinterDialog):
     #@-node:ekr.20031218072017.3908:Tkinter wrappers (leoTkinterFind)
     #@-others
 #@nonl
+#@-node:ekr.20041025152343.1:class leoTkinterFind
+#@-others
+
 #@-node:ekr.20031218072017.3897:@thin leoTkinterFind.py
 #@-leo
