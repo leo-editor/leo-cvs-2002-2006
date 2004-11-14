7c5df6b6f0ff52bdaf66b0c9034f05ee6c976583
edream <>
2004-11-14 16:40:12 +0000

    Clarified style guide by moving many comments into a separate nodes section.

--- leo/plugins/style_guide.py
+++ leo/plugins/style_guide.py
@@ -1,31 +1,17 @@
 #@+leo-ver=4-thin
 #@+node:ekr.20040919081244:@thin style_guide.py
+"""This docstring should be a clear, concise description of
+what the plugin does and how to use it.
 """
-This is the plugin's docstring. Leo prints the __version__ variable (see
-below) and this docstring when the user selects the plugin in the Plugins menu.
-Therefore, this docstring should be a clear, concise description of what the
-plugin does and how to use it.
-
-N.B. Most plugins already follow this style guide to a greater or lesser extent.
-It is probably easier to undersand the purpose of this style guide by studying
-existing plugins. The reason is that this style guide is cluttered by many
-comments that would not appear in actual plugins.
-"""
-
-# This node and its decendents form a style guide for plugins.
-# NOTE 1: Do NOT include either these initial comments or any of Python comments below.
-# Note 2: There is no need to use sections if they are empty.
 
 #@@language python
 #@@tabwidth -4
 
 #@<< about this plugin >>
 #@+node:ekr.20040919082800:<< about this plugin >>
-#@+at
-# 
-# This node should contain a comment, starting with '@' as shown above.
-# 
-# Make sure you tell what your plugin does and how to use it. You may include 
+#@+at 
+#@nonl
+# This section tells what your plugin does and how to use it. You may include 
 # as
 # much detail as you like, but please try to be as clear and concise as 
 # possible.
@@ -56,22 +42,16 @@ __version__ = "1.0"
 #@nl
 #@<< imports >>
 #@+node:ekr.20040919082800.2:<< imports >>
-# The comments in this node would not be part of the actual plugin.
-# Most plugins will include the following imports:
+# Almost all plugins will use these two imports.
 import leoGlobals as g
 import leoPlugins
 
-# Next would follow imports of Leo's modules.
-# Note: it is almost never necessary to import leoNodes.
-
-# Next would follow imports of any standard Python modules.
-# Good Python style is to put each import on a separate line.
-# Examples:
+# Please put imports on a separate line.
+# Please do not use 'from x import y' or 'from x import *'.
 import os
 import sys
 
-# Please do _not_ assume that modules like Tkinter, Pmw, etc. are always available.
-# A good way to test for the precence of such modules is as follows:
+# Shows how to test for modules that may not exist...
 try:
     import Tkinter as Tk
 except ImportError:
@@ -81,13 +61,6 @@ try:
     import Pmw
 except ImportError:
     Tk = g.cantImport("Pmw",__name__)
-    
-# As shown above, I prefer to abbreviate Tkinter as Tk
-# Please do NOT abbreviate Pmw or leoPlugins.
-
-# Please do NOT use either of the following kinds of imports.
-# from m import *
-# from m import x,y,z
 #@nonl
 #@-node:ekr.20040919082800.2:<< imports >>
 #@nl
@@ -107,14 +80,13 @@ except ImportError:
 # to
 # access the data using g.top(). Instead, your plugin should define an 
 # onCreate
-# function as shown in nother nodes. onCreate will create a class instance in
+# function as shown in other nodes. onCreate will create a class instance in
 # which self.c is bound to a single commander.
 #@-at
 #@nonl
 #@-node:ekr.20040919082800.3:<< globals >>
 #@nl
 
-# PLEASE define each function or method in a separate node!
 #@+others
 #@+node:ekr.20040919084039:onCreate
 #@+at
@@ -152,16 +124,16 @@ class myClass:
     A class illustrating how to bind a class instance permanently to a _particular_ window.
     All methods of this class should use self.c rather than c = g.top().
     """
-    
-    # PLEASE define each method in a separate node!
+
     #@    @+others
     #@+node:ekr.20040919084723.1:myClass.__init__
     #@+at
     # 
     # Binding c to self.c as shown ensures that this class always acts on the 
-    # same commander, regardless of what Leo window is presently on top.  This 
-    # is crucial to having your plugin work properly when multiple Leo windows 
-    # are open.
+    # same
+    # commander, regardless of what Leo window is presently on top. This is 
+    # crucial to
+    # having your plugin work properly when multiple Leo windows are open.
     # 
     #@-at
     #@@c
@@ -203,26 +175,16 @@ def onStart2 (tag, keywords):
 #@-node:ekr.20040919085752:onStart2
 #@-others
 
-# You need this if statement ONLY if your plugin depends on modules that might not be available.
-# See the imports section for more discussion.
-if Tk and Pmw:
-    
-    # You need the following three lines ONLY if your plugin uses a gui.
-    # If you don't need them, just use the code following these three lines.
+if Tk and Pmw: # This statement needed only if the plugin uses modules that are not always available.
+    # The following three lines needed only if the plugin uses a gui.
     if g.app.gui is None: 
         g.app.createTkGui(__file__)
     if g.app.gui.guiName() == "tkinter":
-        # If your plugin needs to access data from a commander, it is almost always a bad
-        # idea to use g.top() to get the commander of the presently selected frame.
-        # Instead, use the onCreate function to create a class that binds self.c.
+
+        # Shows how to create a class that binds self.c properly.
         leoPlugins.registerHandler("after-create-leo-frame", onCreate)
-        
-        # If your plugin acts globally (rather than on particular windows)
-        # it is safe to define hook handlers here.  For example:
+        # Shows how to create a hook that doesn't access commanders.
         leoPlugins.registerHandler("start2", onStart2)
-        
-        # Note: use g.plugin_signon, NOT leoPlugins.plugin_signon.
-        # Please do not set __name__ explicitly.
         g.plugin_signon(__name__)
 #@nonl
 #@-node:ekr.20040919081244:@thin style_guide.py
