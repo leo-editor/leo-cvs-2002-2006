9704fb3f50b906b0a07764214ba27a66fec79999
edream <>
2004-12-20 23:04:58 +0000

    Initial checkin.

---
+++ leo/test/cvsConflict.py
@@ -0,0 +1,19 @@
+#@+leo-ver=4-thin
+#@+node:ekr.20041220170413:@thin cvsConflict.py
+# A file to test for cvs conflicts
+
+#@+others
+#@+node:ekr.20041220170413.1:spam
+def spam():
+    pass
+#@nonl
+#@-node:ekr.20041220170413.1:spam
+#@+node:ekr.20041220170413.2:eggs
+def eggs():
+    pass
+#@nonl
+#@-node:ekr.20041220170413.2:eggs
+#@-others
+#@nonl
+#@-node:ekr.20041220170413:@thin cvsConflict.py
+#@-leo
