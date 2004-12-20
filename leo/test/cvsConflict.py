cfa5a4bd441865e70b92f9ecaccad52e83aaab16
edream <>
2004-12-20 23:27:52 +0000

    spam-2a

--- leo/test/cvsConflict.py
+++ leo/test/cvsConflict.py
@@ -3,16 +3,16 @@
 # A file to test for cvs conflicts
 
 #@+others
-#@+node:ekr.20041220170413.1:spam
+#@+node:ekr.20041220170413.1:spam-2b
 def spam2():
     pass
 #@nonl
-#@-node:ekr.20041220170413.1:spam
-#@+node:ekr.20041220170413.2:eggs
+#@-node:ekr.20041220170413.1:spam-2b
+#@+node:ekr.20041220170413.2:eggs-2b
 def eggs():
     pass # cvs2
 #@nonl
-#@-node:ekr.20041220170413.2:eggs
+#@-node:ekr.20041220170413.2:eggs-2b
 #@-others
 #@nonl
 #@-node:ekr.20041220170413:@thin cvsConflict.py
