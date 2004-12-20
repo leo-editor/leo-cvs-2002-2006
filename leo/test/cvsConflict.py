de89d373cf58af34dc97e0b69d1219fe13768b59
edream <>
2004-12-20 23:06:59 +0000

    First change.

--- leo/test/cvsConflict.py
+++ leo/test/cvsConflict.py
@@ -4,13 +4,13 @@
 
 #@+others
 #@+node:ekr.20041220170413.1:spam
-def spam():
+def spam2():
     pass
 #@nonl
 #@-node:ekr.20041220170413.1:spam
 #@+node:ekr.20041220170413.2:eggs
 def eggs():
-    pass
+    pass # cvs2
 #@nonl
 #@-node:ekr.20041220170413.2:eggs
 #@-others
