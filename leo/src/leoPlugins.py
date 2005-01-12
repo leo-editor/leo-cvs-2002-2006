735701b5981147435a1bb73b4cd07e8d50a02a14
edream <>
2005-01-12 15:32:27 +0000

    Added unloadOnePlugin. (Not too useful.) Associated module names with handlers. Used g.Bunch for handler info.

--- leo/src/leoPlugins.py
+++ leo/src/leoPlugins.py
@@ -23,16 +23,15 @@ import leoGlobals as g
 
 handlers = {}
 loadedModules = {} # Keys are module names, values are modules.
+loadingModuleNameStack = [] # The stack of module names.  Top is the module being loaded.
 
 #@+others
 #@+node:ekr.20050102094729:callTagHandler
-def callTagHandler (handler,tag,keywords):
+def callTagHandler (bunch,tag,keywords):
+    
+    handler = bunch.fn ; moduleName = bunch.moduleName
     
     if tag == 'idle':
-        
-        if 0:
-            c = keywords.get('c')
-            g.trace(c.shortFileName(),handler)
 
         # Make sure all commanders exist.
         for key in ('c','old_c','new_c'):
@@ -44,10 +43,44 @@ def callTagHandler (handler,tag,keywords):
                 except AttributeError:
                     # c has been destroyed: c.frame ivar does not exist.
                     return None
-
-    return handler(tag,keywords)
+                    
+    # Calls to registerHandler from inside the handler belong to moduleName.
+    global loadingModuleNameStack
+    loadingModuleNameStack.append(moduleName)
+    result = handler(tag,keywords)
+    loadingModuleNameStack.pop()
+    return result
 #@nonl
 #@-node:ekr.20050102094729:callTagHandler
+#@+node:ekr.20031218072017.3442:doHandlersForTag
+def doHandlersForTag (tag,keywords):
+    
+    """Execute all handlers for a given tag, in alphabetical order.
+    
+    All exceptions are caught by the caller, doHook."""
+
+    global handlers
+
+    if g.app.killed:
+        return None
+
+    if handlers.has_key(tag):
+        bunches = handlers.get(tag)
+        # Execute hooks in some random order.
+        # Return if one of them returns a non-None result.
+        for bunch in bunches:
+            val = callTagHandler(bunch,tag,keywords)
+            if val is not None:
+                return val
+
+    if handlers.has_key("all"):
+        bunches = handlers.get('all')
+        for bunch in bunches:
+            callTagHandler(bunch.fn,tag,keywords)
+
+    return None
+#@nonl
+#@-node:ekr.20031218072017.3442:doHandlersForTag
 #@+node:ekr.20041001161108:doPlugins
 def doPlugins(tag,keywords):
     if g.app.killed:
@@ -58,6 +91,42 @@ def doPlugins(tag,keywords):
     return doHandlersForTag(tag,keywords)
 #@nonl
 #@-node:ekr.20041001161108:doPlugins
+#@+node:ekr.20041111124831:getHandlersForTag
+def getHandlersForTag(tags):
+    
+    import types
+
+    if type(tags) in (types.TupleType,types.ListType):
+        result = []
+        for tag in tags:
+            fn = getHandlersForOneTag(tag) 
+            result.append((tag,fn),)
+        return result
+    else:
+        return getHandlersForOneTag(tags)
+
+def getHandlersForOneTag (tag):
+
+    global handlers
+
+    bunch = handlers.get(tag)
+    return bunch.fn
+#@nonl
+#@-node:ekr.20041111124831:getHandlersForTag
+#@+node:ekr.20041114113029:getPluginModule
+def getPluginModule (moduleName):
+    
+    global loadedModules
+    
+    return loadedModules.get(moduleName)
+#@nonl
+#@-node:ekr.20041114113029:getPluginModule
+#@+node:ekr.20041001160216:isLoaded
+def isLoaded (name):
+    
+    return name in g.app.loadedPlugins
+#@nonl
+#@-node:ekr.20041001160216:isLoaded
 #@+node:ekr.20031218072017.3440:loadHandlers
 def loadHandlers(loadAllFlag=False):
 
@@ -120,6 +189,7 @@ def loadOnePlugin (moduleOrFileName, verbose=False):
         moduleName = moduleOrFileName [:-3]
     else:
         moduleName = moduleOrFileName
+    moduleName = g.shortFileName(moduleName)
 
     if isLoaded(moduleName):
         module = loadedModules.get(moduleName)
@@ -127,110 +197,50 @@ def loadOnePlugin (moduleOrFileName, verbose=False):
 
     plugins_path = g.os_path_join(g.app.loadDir,"..","plugins")
     moduleName = g.toUnicode(moduleName,g.app.tkEncoding)
+    
+    # This import typically results in calls to registerHandler.
+    global loadingModuleNameStack
+    loadingModuleNameStack.append(moduleName)
     result = g.importFromPath(moduleName,plugins_path)
+    loadingModuleNameStack.pop()
+
     if result:
         loadedModules[moduleName] = result
     
     if verbose:
         if result is None:
-            g.es("can not load %s plugin" % moduleName,color="blue")
+            s = 'can not load %s plugin' % moduleName
+            print s ; g.es(s,color="blue")
         else:
-            g.es("loaded %s plugin" % moduleName,color="blue")
+            print 'loaded %s plugin' % moduleName
     
     return result
-#@nonl
 #@-node:ekr.20041113113140:loadOnePlugin
-#@+node:ekr.20031218072017.3442:doHandlersForTag
-def doHandlersForTag (tag,keywords):
+#@+node:ekr.20050110191444:printHandlers
+def printHandlers (moduleName=None):
     
-    """Execute all handlers for a given tag, in alphabetical order.
-    
-    All exceptions are caught by the caller, doHook."""
-
-    global handlers
-
-    if g.app.killed:
-        return None
-
-    if handlers.has_key(tag):
-        fns = handlers.get(tag)
-        # Execute hooks in some random order.
-        # Return if one of them returns a non-None result.
-        for fn in fns:
-            val = callTagHandler(fn,tag,keywords)
-            if val is not None:
-                return val
-
-    if handlers.has_key("all"):
-        fns = handlers.get("all")
-        for fn in fns:
-            callTagHandler(fn,tag,keywords)
-
-    return None
-#@nonl
-#@-node:ekr.20031218072017.3442:doHandlersForTag
-#@+node:ekr.20041001160216:isLoaded
-def isLoaded (name):
-    
-    return name in g.app.loadedPlugins
-#@nonl
-#@-node:ekr.20041001160216:isLoaded
-#@+node:ekr.20041111124831:getHandlersForTag
-def getHandlersForTag(tags):
-    
-    import types
-
-    if type(tags) in (types.TupleType,types.ListType):
-        result = []
-        for tag in tags:
-            fn = getHandlersForOneTag(tag)
-            result.append((tag,fn),)
-        return result
+    if moduleName:
+        print 'handlers for %s...' % moduleName
     else:
-        return getHandlersForOneTag(tags)
+        print 'all plugin handlers...'
 
-def getHandlersForOneTag (tag):
-
-    global handlers
-    
-    return handlers.get(tag)
+    modules = {}
+    for tag in handlers.keys():
+        bunches = handlers.get(tag)
+        for bunch in bunches:
+            name = bunch.moduleName
+            tags = modules.get(name,[])
+            tags.append(tag)
+            modules[name] = tags
+    keys = modules.keys()
+    keys.sort()
+    for key in keys:
+        tags = modules.get(key)
+        if moduleName in (None,key):
+            for tag in tags:
+                print '%25s %s' % (tag,key)
 #@nonl
-#@-node:ekr.20041111124831:getHandlersForTag
-#@+node:ekr.20041114113029:getPluginModule
-def getPluginModule (moduleName):
-    
-    global loadedModules
-    
-    return loadedModules.get(moduleName)
-#@nonl
-#@-node:ekr.20041114113029:getPluginModule
-#@+node:ekr.20031218072017.3443:registerHandler
-def registerHandler(tags,fn):
-    
-    """ Register one or more handlers"""
-    
-    import types
-
-    if type(tags) in (types.TupleType,types.ListType):
-        for tag in tags:
-            registerOneHandler(tag,fn)
-    else:
-        registerOneHandler(tags,fn)
-
-def registerOneHandler(tag,fn):
-    
-    """Register one handler"""
-
-    global handlers
-    
-    items = handlers.get(tag,[])
-    if fn not in items:
-        items.append(fn)
-        
-    # g.trace(tag) ; g.printList(items)
-    handlers[tag] = items
-#@nonl
-#@-node:ekr.20031218072017.3443:registerHandler
+#@-node:ekr.20050110191444:printHandlers
 #@+node:ekr.20031218072017.3444:registerExclusiveHandler
 def registerExclusiveHandler(tags, fn):
     
@@ -248,14 +258,76 @@ def registerOneExclusiveHandler(tag, fn):
     
     """Register one exclusive handler"""
     
-    global handlers
+    global handlers, loadingModuleNameStack
+    moduleName = loadingModuleNameStack[-1]
     
+    if 0:
+        if g.app.unitTesting: print
+        print '%6s %15s %25s %s' % (g.app.unitTesting,moduleName,tag,fn.__name__)
+    
+    if g.app.unitTesting: return
+
     if handlers.has_key(tag):
         g.es("*** Two exclusive handlers for '%s'" % tag)
     else:
-        handlers[tag] = (fn,)
+        bunch = g.Bunch(fn=fn,moduleName=moduleName,tag='handler')
+        handlers = [bunch]
 #@nonl
 #@-node:ekr.20031218072017.3444:registerExclusiveHandler
+#@+node:ekr.20031218072017.3443:registerHandler
+def registerHandler(tags,fn):
+    
+    """ Register one or more handlers"""
+
+    import types
+
+    if type(tags) in (types.TupleType,types.ListType):
+        for tag in tags:
+            registerOneHandler(tag,fn)
+    else:
+        registerOneHandler(tags,fn)
+
+def registerOneHandler(tag,fn):
+    
+    """Register one handler"""
+    
+    global handlers, loadingModuleNameStack
+    moduleName = loadingModuleNameStack[-1]
+    
+    if 0:
+        if g.app.unitTesting: print
+        print '%6s %15s %25s %s' % (g.app.unitTesting,moduleName,tag,fn.__name__)
+
+    items = handlers.get(tag,[])
+    if fn not in items:
+        
+        bunch = g.Bunch(fn=fn,moduleName=moduleName,tag='handler')
+        items.append(bunch)
+        
+    # g.trace(tag) ; g.printList(items)
+    handlers[tag] = items
+#@nonl
+#@-node:ekr.20031218072017.3443:registerHandler
+#@+node:ekr.20050110182317:unloadOnePlugin
+def unloadOnePlugin (moduleOrFileName,verbose=False):
+    
+    if moduleOrFileName [-3:] == ".py":
+        moduleName = moduleOrFileName [:-3]
+    else:
+        moduleName = moduleOrFileName
+    moduleName = g.shortFileName(moduleName)
+
+    if moduleName in g.app.loadedPlugins:
+        if verbose:
+            print 'unloading',moduleName
+        g.app.loadedPlugins.remove(moduleName)
+        
+    for tag in handlers.keys():
+        bunches = handlers.get(tag)
+        bunches = [bunch for bunch in bunches if bunch.moduleName != moduleName]
+        handlers[tag] = bunches
+#@nonl
+#@-node:ekr.20050110182317:unloadOnePlugin
 #@+node:ekr.20041111123313:unregisterHandler
 def unregisterHandler(tags,fn):
     
@@ -270,13 +342,18 @@ def unregisterHandler(tags,fn):
 def unregisterOneHandler (tag,fn):
 
     global handlers
-
-    fn_list = handlers.get(tag)
-    if fn_list:
-        while fn in fn_list:
-            fn_list.remove(fn)
-        handlers[tag] = fn_list
-        # g.trace(handlers.get(tag))
+    
+    if 1: # New code
+        bunches = handlers.get(tag)
+        bunches = [bunch for bunch in bunches if bunch.fn != fn]
+        handlers[tag] = bunches
+    else:
+        fn_list = handlers.get(tag)
+        if fn_list:
+            while fn in fn_list:
+                fn_list.remove(fn)
+            handlers[tag] = fn_list
+            # g.trace(handlers.get(tag))
 #@nonl
 #@-node:ekr.20041111123313:unregisterHandler
 #@-others
