3757bfd8c01aea7038452ff03c4444c7ab3a2e5b
edream <>
2005-02-20 00:58:03 +0000

    - Added commander-specific callback in onCreate. - Added init method.

--- leo/plugins/xemacs.py
+++ leo/plugins/xemacs.py
@@ -1,38 +1,99 @@
 #@+leo-ver=4-thin
 #@+node:EKR.20040517075715.12:@thin xemacs.py
-"""emacs handler
+'''This plugin allows you to edit nodes in emacs/xemacs.
 
-http://www.cs.mu.oz.au/~markn/leo/external_editors.leo"""
+Depending on your preference, selecting or double-clicking a node will pass the
+body text of that node to emacs. You may edit the node in the emacs buffer and
+changes will appear in Leo. '''
 
 #@@language python
 #@@tabwidth -4
 
+#@<< imports >>
+#@+node:ekr.20050218024153:<< imports >>
 import leoGlobals as g
 import leoPlugins
 import sys
+#@nonl
+#@-node:ekr.20050218024153:<< imports >>
+#@nl
+__version__ = "1.5"
+#@<< version history >>
+#@+node:ekr.20050218024153.1:<< version history >>
+#@@killcolor
+#@+at
+# 
+# Initial version: http://www.cs.mu.oz.au/~markn/leo/external_editors.leo
+# 
+# 1.5 EKR:
+#     - Added commander-specific callback in onCreate.
+#     - Added init method.
+#@-at
+#@nonl
+#@-node:ekr.20050218024153.1:<< version history >>
+#@nl
 
+use_double_click = True
+    # True:  Edit when double-clicking a node's icon.
+    # False: Edit when selecting any node (a bit much for my taste).
+
+# Set this to the path to emacs/xemacs.
 if sys.platform == "win32":
-    # Modify this pth as needed.
-    path = r"c:\Program Files\XEmacs\XEmacs-21.4.13\i586-pc-win32\xemacs.exe"
-else: # Linux
-    path = "/usr/bin/gnuclient"
+    # N.B.  This path must not contain blanks in XP.  Sheesh.
+    path_to_emacs = r"c:\XEmacs\XEmacs-21.4.13\i586-pc-win32\xemacs.exe"
+    #g.trace('norm',g.os_path_normpath(path_to_emacs))
+    #g.trace('abs ',g.os_path_abspath(path_to_emacs))
+else:
+    path_to_emacs = "/usr/bin/gnuclient"
 
-def open_in_emacs (tag,keywords):
-    g.top() and g.top().openWith(("os.spawnl",path,None),)
-
-# Ok for unit testing, but you might want to disable it.
-if 1 or not g.app.unitTesting:
+#@+others
+#@+node:ekr.20050218023308:init
+def init ():
     
-    if g.app.unitTesting:
-        print '\nxemacs plugin installed: double-clicking icons will start xemacs.'
+    ok = g.os_path_exists(g.os_path_abspath(path_to_emacs))
 
-    if 1: # Edit when double-clicking a node's icon.
-        leoPlugins.registerHandler("icondclick2", open_in_emacs) # Activate on _double_ click.
-    else: # Edit when selecting any node. That's a bit much for my taste.
-        leoPlugins.registerHandler("select2", open_in_emacs)
+    if ok:
+        if g.app.unitTesting: # Probably will never happen now...
+            print '\nxemacs plugin installed: double-clicking icons will start xemacs.'
+
+        leoPlugins.registerHandler("after-create-leo-frame",onCreate)
+        g.plugin_signon(__name__)
+        
+    else:
+        print 'bad path to emacs:',path_to_emacs
+
+    return ok
+#@nonl
+#@-node:ekr.20050218023308:init
+#@+node:ekr.20050218023308.1:onCreate
+def onCreate (tag,keywords):
+
+    c = keywords.get("c")
+    if not c: return
     
-    __version__ = "1.4"
-    g.plugin_signon(__name__)
+    # Define a commander-specific callback.
+    def open_in_emacs_callback(*args):
+        open_in_emacs(c)
+        
+    handler = g.choose(use_double_click,'icondclick2','select2')
+    
+    leoPlugins.registerHandler(handler,open_in_emacs_callback)
+#@nonl
+#@-node:ekr.20050218023308.1:onCreate
+#@+node:ekr.20050218032201:open_in_emacs
+def open_in_emacs(c):
+
+    p = c.currentPosition()
+    if not p: return
+
+    if 0:
+        # Path must end in a blank!
+        c.openWith(('os.system',path_to_emacs+' ',None),)
+    else:
+        c.openWith(("os.spawnl",path_to_emacs,None),)
+#@nonl
+#@-node:ekr.20050218032201:open_in_emacs
+#@-others
 #@nonl
 #@-node:EKR.20040517075715.12:@thin xemacs.py
 #@-leo
