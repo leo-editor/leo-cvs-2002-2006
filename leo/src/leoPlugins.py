073ecacb4ae5eac6f96586816eb1a1a36bb34e1f
edream <>
2005-05-08 21:25:47 +0000

    Corrected recent bugs in callTagHandler. - Only check for existence of commanders for 'idle' tags. - Only check 'c' and 'new_c' keys.

--- leo/src/leoPlugins.py
+++ leo/src/leoPlugins.py
@@ -32,14 +32,14 @@ def callTagHandler (bunch,tag,keywords):
     
     handler = bunch.fn ; moduleName = bunch.moduleName
 
-    # Make sure all commanders exist.
-    # New in 4.3 b2: this can be a problem for any tag.
-    for key in ('c','old_c','new_c'):
-        c = keywords.get(key)
-        if c:
-            if not hasattr(c,'frame'): return None
-            if c.frame not in g.app.windowList:
-                return None # c has (or will be) destroyed.
+    # Make sure the new commander exists.
+    if tag == 'idle':
+        for key in ('c','new_c'):
+            c = keywords.get(key)
+            if c:
+                if not hasattr(c,'frame'): return None
+                if c.frame not in g.app.windowList:
+                    return None # c has (or will be) destroyed.
 
     # Calls to registerHandler from inside the handler belong to moduleName.
     global loadingModuleNameStack
