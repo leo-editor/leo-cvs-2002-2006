29b2021696ca65b9ab2926c6407d3b89995e6e24
edream <>
2005-05-13 12:42:10 +0000

    Fixed bug that caused scrollbar to be hidden in a brand-new window that is split horizontally. It also honors the initial ratio settings.

--- leo/src/leo.py
+++ leo/src/leo.py
@@ -205,6 +205,7 @@ def createFrame (fileName):
     # Create a _new_ frame & indicate it is the startup window.
     c,frame = g.app.gui.newLeoCommanderAndFrame(fileName=fileName)
     frame.setInitialWindowGeometry()
+    frame.resizePanesToRatio(frame.ratio,frame.secondary_ratio) # 5/13/05
     frame.startupWindow = True
     # 3/2/05: Call the 'new' hook for compatibility with plugins.
     g.doHook("new",old_c=None,c=c,new_c=c)
