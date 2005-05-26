f8754ecf9af2b1c7f1cca786b9ab13fc0ee1aa03
edream <>
2005-05-26 17:20:05 +0000

    v 0.2 by EKR - Added init method. - Simplified the code.
    ** Please base all future work on this version.
    
    The code appears to work, however the binding to <Button-1> causes conflcts with other events in the Tree.
    The solution is to use another binding.

--- leo/plugins/UniversalScrolling.py
+++ leo/plugins/UniversalScrolling.py
@@ -1,10 +1,8 @@
 #@+leo-ver=4-thin
 #@+node:ekr.20040831122004:@thin UniversalScrolling.py
-"""
-A plugin that enables the user to scroll down with a left mouse click and
-hold, and to scroll up with a right mouse click and hold. Releasing in both
-cases stops the scrolling.
-"""
+""" A plugin that enables the user to scroll down with a left mouse click and
+hold, and to scroll up with a right mouse click and hold. Scrolling continues
+until the user releases the mouse. """
 
 #@<< about this plugin >>
 #@+node:ekr.20040915104230.5:<< about this plugin >>
@@ -31,87 +29,126 @@ import leoTkinterFrame
 
 Tk = g.importExtension('Tkinter',pluginName=__name__,verbose=True)
     
+import threading
 import time
 #@nonl
 #@-node:ekr.20050101090207.5:<< imports >>
+#@nl
+__version__ = "0.2"
+#@<< version history >>
+#@+node:ekr.20050526121340:<< version history >>
+#@@nocolor
+
+#@+at
+# 
+# 0.2 EKR:
+#     - Added init method.
+#     - Simplified the code.
+# 
+# Note: This code appears to work. At present it suffers from re-binding 
+# Button-1.
+# The solution is to pick another binding :-)
+#@-at
+#@nonl
+#@-node:ekr.20050526121340:<< version history >>
 #@nl
 
 createCanvas = leoTkinterFrame.leoTkinterFrame.createCanvas
 
 #@+others
+#@+node:ekr.20050526121026:init
+def init ():
+    
+    ok = Tk and not g.app.unitTesting
+    
+    if ok:
+        if g.app.gui is None:
+            g.app.createTkGui(__file__)
+            
+        ok = g.app.gui.guiName() == "tkinter"
+
+        if ok:
+            leoTkinterFrame.leoTkinterFrame.createCanvas = addUThreading
+            g.plugin_signon(__name__)
+        
+    return ok
+#@nonl
+#@-node:ekr.20050526121026:init
 #@+node:ekr.20040915104230:addUThreading & callbacks
-def addUThreading( self, parentFrame ):
+def addUThreading (tkFrame,parentFrame):
 
     '''Replaces createCanvas, adding UniversalScolling'''
-
-    canvas = createCanvas( self, parentFrame )
-    import threading
-    ev = threading.Event()
-
-    class direction:
+    
+    class directionClass:
         way = 'Down'
-
-    way = direction()
+        
+    canvas = createCanvas(tkFrame,parentFrame)
+    ev = threading.Event()
+    presentDirection = directionClass()
     
     # Define callbacks.
     #@    @+others
     #@+node:ekr.20040915104230.2:run
-    #here we watch for events to begin scrolling.
-    #it is also the target of the thread.
-    def run( ev = ev , way = way):
-        
+    # Watch for events to begin scrolling.  This is also the target of the thread.
+    def run( ev = ev):
+    
         while 1:
             ev.wait()
-            if way.way == 'Down':
+            # g.trace(presentDirection.way)
+            if presentDirection.way == 'Down':
+                # g.trace(canvas.yview())
                 canvas.yview(Tk.SCROLL, 1, Tk.UNITS)
             else:
                 canvas.yview( Tk.SCROLL, -1, Tk.UNITS )
-            time.sleep( .1 )
-    
-    t = threading.Thread( target = run )
-    t.setDaemon( True )
-    t.start()
-    #@nonl
+            time.sleep(.1)
     #@-node:ekr.20040915104230.2:run
-    #@+node:ekr.20040915104230.3:exe
-    def exe( event, ev = ev, wy = 'Down', way = way ):
-        
+    #@+node:ekr.20050526094449.2:scroll & helpers
+    def scroll(event,way):
+    
         """A callback that starts scrolling."""
     
         x = event.widget.canvasx( event.x )
         y = event.widget.canvasy( event.y )
+        item = event.widget.find_overlapping(x,y,x,y)
+        if item:
+            return "continue"
+        else:
+            # g.trace(event,way,event.widget)
+            ev.set()
+            presentDirection.way = way
+            return 'break'
     
-        if event.widget.find_overlapping( x,y,x,y): 
-            return
-    
-        ev.set()
-        way.way = wy 
-        return 'break'
+    def scrollDown(event):
+        scroll(event,'Down')
+        
+    def scrollUp(event):
+        scroll(event,'Up')
     #@nonl
-    #@-node:ekr.20040915104230.3:exe
-    #@+node:ekr.20040915104230.4:off
-    def off( event, ev = ev ):
+    #@-node:ekr.20050526094449.2:scroll & helpers
+    #@+node:ekr.20040915104230.4:stopScrolling
+    def stopScrolling (event,ev=ev):
         
         """A callback that stops scrolling."""
     
         ev.clear()
     #@nonl
-    #@-node:ekr.20040915104230.4:off
+    #@-node:ekr.20040915104230.4:stopScrolling
     #@-others
+    
+    # Start the thread.
+    t = threading.Thread(target = run)
+    t.setDaemon(True)
+    t.start()
 
     # Replace the canvas bindings.
-    canvas.bind( '<Button-1>', exe )
-    canvas.bind( '<Button-3>', lambda event, way = 'Up': exe( event, wy = way ) )
-    canvas.bind( '<ButtonRelease-1>', off )
-    canvas.bind( '<ButtonRelease-3>', off )
+    canvas.bind( '<Button-1>', scrollDown)
+    canvas.bind( '<Button-3>', scrollUp)
+    canvas.bind( '<ButtonRelease-1>', stopScrolling)
+    canvas.bind( '<ButtonRelease-3>', stopScrolling)
     return canvas
 #@nonl
 #@-node:ekr.20040915104230:addUThreading & callbacks
 #@-others
-
-# This probably is not ready for 4.3.
-if not g.app.unitTesting: # Not for unit test: Adds binding to Tk canvas.
-    leoTkinterFrame.leoTkinterFrame.createCanvas = addUThreading 
 #@nonl
 #@-node:ekr.20040831122004:@thin UniversalScrolling.py
 #@-leo
