4cde0f50a5b2e8ee496530c5477522884e766ee4
edream <>
2005-10-15 17:57:14 +0000

    Updated version to 4.4a1.

--- leo/src/leoCommands.py
+++ leo/src/leoCommands.py
@@ -259,7 +259,7 @@ class baseCommands:
     #@+node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     def getSignOnLine (self):
         c = self
-        return "Leo 4.3.3, build %s, September 17, 2005" % c.getBuildNumber()
+        return "Leo 4.4 alpha 1, build %s, October 15, 2005" % c.getBuildNumber()
     #@nonl
     #@-node:ekr.20040629121554.1:getSignOnLine (Contains hard-coded version info)
     #@+node:ekr.20040629121554.2:initVersion
