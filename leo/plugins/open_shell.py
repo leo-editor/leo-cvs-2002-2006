39b2465ec80c0795735da479a6fa29fc6835d658
edream <>
2006-01-07 17:06:04 +0000

    0.7 EKR: (Removed g.top) - Added init function.  - Created per-commander pluginController class.

--- leo/plugins/open_shell.py
+++ leo/plugins/open_shell.py
@@ -23,7 +23,7 @@ Current limitations:
 #@@language python
 #@@tabwidth -4
 
-__version__ = "0.6"
+__version__ = "0.7"
 
 #@<< version history >>
 #@+node:ekr.20040909100119:<< version history >>
@@ -35,6 +35,9 @@ __version__ = "0.6"
 # directives.
 # 0.6 EKR:
 #     - Moved most docs into the docstring.
+# 0.7 EKR: (Removed g.top)
+#     - Added init function.
+#     - Created per-commander pluginController class.
 #@-at
 #@nonl
 #@-node:ekr.20040909100119:<< version history >>
@@ -62,82 +65,112 @@ else:
     pathToCmd = ''
 
 #@+others
-#@+node:EKR.20040517080049.6:load_menu
-def load_menu(tag,keywords):
-    
-    if sys.platform=="win32":
-        table = (
-            ("&Open Console Window",None,launchCmd),
-            ("Open &Explorer",None,launchExplorer)) 
-    else:
-        table = ( ("Open &xterm",None,launchxTerm), ) 
-    
-    g.top().frame.menu.createNewMenu("E&xtensions","top")
-    g.top().frame.menu.createMenuItemsFromTable("Extensions",table)
-#@-node:EKR.20040517080049.6:load_menu
-#@+node:EKR.20040517080049.7:_getpath
-def _getpath(c,v):
+#@+node:ekr.20060107110126:init
+def init ():
 
-    dict = g.scanDirectives(c,p=v)
-    d = dict.get("path")
+    if 1: # Ok for unit testing: creates a new menu.
     
-    if v.isAnyAtFileNode():
-        filename = v.anyAtFileNodeName()
-        filename = g.os_path_join(d,filename)
-        if filename:
-            d = g.os_path_dirname(filename)
-
-    if d is None:
-        return ""
-    else:
-        return g.os_path_normpath(d)
+        # Register the handlers...
+        leoPlugins.registerHandler("after-create-leo-frame", onCreate)
+    
+        g.plugin_signon(__name__)
 #@nonl
-#@-node:EKR.20040517080049.7:_getpath
-#@+node:EKR.20040517080049.8:_getCurrentNodePath
-def _getCurrentNodePath():
-
-    c = g.top()
-    v = c.currentVnode()
-    d = _getpath(c,v)
-    return d
-#@-node:EKR.20040517080049.8:_getCurrentNodePath
-#@+node:EKR.20040517080049.9:launchCmd
-def launchCmd(event=None):
+#@-node:ekr.20060107110126:init
+#@+node:ekr.20060107110126.1:onCreate
+def onCreate (tag, keywords):
     
-    global pathToCmd
-
-    d = _getCurrentNodePath()
-    myCmd = 'cd ' + d
-    os.spawnl(os.P_NOWAIT, pathToCmd, '/k ', myCmd)
+    c = keywords.get('c')
+    if c:
+        controller = pluginController(c)
+        controller.load_menu()
 #@nonl
-#@-node:EKR.20040517080049.9:launchCmd
-#@+node:EKR.20040517080049.10:launchExplorer
-def launchExplorer(event=None):
+#@-node:ekr.20060107110126.1:onCreate
+#@+node:ekr.20060107110126.2:class pluginController
+class pluginController:
     
-    global pathToExplorer
-
-    d = _getCurrentNodePath()
-    os.spawnl(os.P_NOWAIT,pathToExplorer, ' ', d)
-
-#@-node:EKR.20040517080049.10:launchExplorer
-#@+node:EKR.20040517080049.11:launchxTerm
-def launchxTerm(not_used):
-
-    d = _getCurrentNodePath()
-    curdir = os.getcwd()
-    os.chdir(d)
-    os.spawnlp(os.P_NOWAIT, 'xterm', '-title Leo')
-    os.chdir(curdir)
+    #@    @+others
+    #@+node:ekr.20060107110126.3:ctor
+    def __init__ (self,c):
+        
+        self.c = c
+    #@nonl
+    #@-node:ekr.20060107110126.3:ctor
+    #@+node:EKR.20040517080049.6:load_menu
+    def load_menu (self):
+    
+        if sys.platform == "win32":
+            table = (
+                ("&Open Console Window",None,self.launchCmd),
+                ("Open &Explorer",None,self.launchExplorer))
+        else:
+            table = (("Open &xterm",None,self.launchxTerm),)
+    
+        c = self.c
+        c.frame.menu.createNewMenu("E&xtensions","top")
+        c.frame.menu.createMenuItemsFromTable("Extensions",table)
+    #@nonl
+    #@-node:EKR.20040517080049.6:load_menu
+    #@+node:EKR.20040517080049.7:_getpath
+    def _getpath (self,p):
+    
+        c = self.c
+        dict = g.scanDirectives(c,p=p)
+        d = dict.get("path")
+    
+        if p.isAnyAtFileNode():
+            filename = p.anyAtFileNodeName()
+            filename = g.os_path_join(d,filename)
+            if filename:
+                d = g.os_path_dirname(filename)
+    
+        if d is None:
+            return ""
+        else:
+            return g.os_path_normpath(d)
+    #@nonl
+    #@-node:EKR.20040517080049.7:_getpath
+    #@+node:EKR.20040517080049.8:_getCurrentNodePath
+    def _getCurrentNodePath(self):
+    
+        c = self.c
+        p = c.currentPosition()
+        d = self._getpath(p)
+        return d
+    #@nonl
+    #@-node:EKR.20040517080049.8:_getCurrentNodePath
+    #@+node:EKR.20040517080049.9:launchCmd
+    def launchCmd(self,event=None):
+        
+        global pathToCmd
+    
+        d = self._getCurrentNodePath()
+        myCmd = 'cd ' + d
+        os.spawnl(os.P_NOWAIT, pathToCmd, '/k ', myCmd)
+    #@nonl
+    #@-node:EKR.20040517080049.9:launchCmd
+    #@+node:EKR.20040517080049.10:launchExplorer
+    def launchExplorer(self,event=None):
+        
+        global pathToExplorer
+    
+        d = self._getCurrentNodePath()
+        os.spawnl(os.P_NOWAIT,pathToExplorer, ' ', d)
+    
+    #@-node:EKR.20040517080049.10:launchExplorer
+    #@+node:EKR.20040517080049.11:launchxTerm
+    def launchxTerm(self,event=None):
+    
+        d = self._getCurrentNodePath()
+        curdir = os.getcwd()
+        os.chdir(d)
+        os.spawnlp(os.P_NOWAIT, 'xterm', '-title Leo')
+        os.chdir(curdir)
+    #@nonl
+    #@-node:EKR.20040517080049.11:launchxTerm
+    #@-others
 #@nonl
-#@-node:EKR.20040517080049.11:launchxTerm
+#@-node:ekr.20060107110126.2:class pluginController
 #@-others
-
-if 1: # Ok for unit testing: creates a new menu.
-
-    # Register the handlers...
-    leoPlugins.registerHandler("after-create-leo-frame", load_menu)
-
-    g.plugin_signon(__name__)
 #@nonl
 #@-node:EKR.20040517080049.4:@thin open_shell.py
 #@-leo
