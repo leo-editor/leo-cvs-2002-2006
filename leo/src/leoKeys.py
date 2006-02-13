c1eb22bc94922cc6c604f0f283fb2e307b900590
edream <>
2006-02-13 20:29:58 +0000

    Added disabled trace.

--- leo/src/leoKeys.py
+++ leo/src/leoKeys.py
@@ -2107,9 +2107,7 @@ class keyHandlerClass:
         alt   = (state & 0x20000) == 0x20000 or (state & 8) == 8 or (state & 0x80) == 0x80
         plain = len(keysym) == 1 # E.g., for ctrl-v the keysym is 'v' but ch is empty.
         
-        # g.es('state: %x' % state)
-        
-        # g.trace('ch',repr(ch),'keysym',repr(keysym))
+        # g.trace('ch',repr(ch),'keysym',repr(keysym),'state: %x' % state)
         
         # The big aha: we can ignore the shift state.
         if plain:
